3115af79406606fee2301aee80c30eed99eaf545
Fidelis Agoundjekpo <fidelisnicolas@gmail.com>
2024-06-22 19:41:18 +0100

    Conditional Statement

---
+++ conditional_statement/complete_Calculator.py
@@ -0,0 +1,8 @@
+firstNum = float(input("Enter the first Number: "))
+secondNum = float(input("Enter the Second number: "))
+operator = input("Enter the operator (+ - / * pow %): ")
+
+if operator == "+":
+    print(firstNum + secondNum)
+elif operator == "-":
+    print(firstNum - secondNum)
---
+++ conditional_statement/conditionIntro.py
@@ -0,0 +1,7 @@
+user_pin = input("Enter your pin: ")
+main_pin = "12345"
+
+if user_pin == main_pin:
+    print("Welcome")
+else:
+    print("Invalid pin")
